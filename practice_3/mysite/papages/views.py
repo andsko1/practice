31749523989e2eeb99ce7afbcdca1c07b3e79e1e
andsko <andsko@cs.spbstu.ru>
2013-10-19 02:03:26 +0400

    3 задание

--- practice_3/mysite/papages/views.py
+++ practice_3/mysite/papages/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-import os
+import os, time
 
 from django.shortcuts import render_to_response
 
@@ -12,7 +12,7 @@ def listing(request, path):
     for i in allfiles:
         fullpath = base + path + "/" + i
         if(os.path.isfile(fullpath)):
-            files.append(i)
+            files.append( { "name": i, "size": str(os.path.getsize(fullpath))+' B', "lastmodtime": time.ctime(os.path.getmtime(fullpath))  } )
         elif(os.path.isdir(fullpath)):
-            dirs.append(i)       
+            dirs.append( { "name": i, "size": str(os.path.getsize(fullpath))+' B', "lastmodtime": time.ctime(os.path.getmtime(fullpath))  } )        
     return render_to_response('listing.html', {'directory': base + path, 'file_list': files, 'dir_list': dirs}) 
