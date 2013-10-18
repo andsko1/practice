68a79317d3a3600d5e69ac5bcbc9449cd5b2fa8d
andsko <andsko@cs.spbstu.ru>
2013-10-19 01:08:00 +0400

    2 задание

--- practice_3/mysite/papages/views.py
+++ practice_3/mysite/papages/views.py
@@ -7,5 +7,12 @@ def listing(request, path):
     base = '/var/log/'
 
     allfiles = os.listdir(base + path)
-
-    return render_to_response('listing.html', {'directory': base + path, 'file_list': allfiles}) 
+    files = []
+    dirs = []
+    for i in allfiles:
+        fullpath = base + path + "/" + i
+        if(os.path.isfile(fullpath)):
+            files.append(i)
+        elif(os.path.isdir(fullpath)):
+            dirs.append(i)       
+    return render_to_response('listing.html', {'directory': base + path, 'file_list': files, 'dir_list': dirs}) 
